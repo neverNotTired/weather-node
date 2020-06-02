17361e354daceb5589626135427e28cb2c6b837a
John <john@pebbledesign.com.au>
2020-06-02 19:12:09 +1000

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,38 @@
+
+console.log('Client side javascript file is loaded!')
+
+const form = document.querySelector('form')
+const searchEl = document.querySelector('input')
+const loader = document.querySelector('p.load')
+const messageOne = document.querySelector('p.p1')
+const messageTwo = document.querySelector('p.p2')
+
+messageTwo.textContent = ''
+messageTwo.textContent = ''
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    loader.style.display = 'block'
+    messageOne.textContent = ''
+    messageTwo.textContent = ''
+    const location = searchEl.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response)=>{
+        response.json().then((data)=>{
+        if (data.error) {
+            console.log(data.error)
+            loader.style.display = 'none'
+            messageOne.textContent = data.error
+            messageTwo.textContent = ''
+            } else {
+                loader.style.display = 'none'
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
+})
+
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const foreCast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/8e54f323365c5e56af1b7f8b0e796efa/'+latitude+','+longitude+'?units=si';
+    // const url = 'https://api.darksky.net/forecast/8e54f323365c5e56af1b7f8b0e796efa/-26.5712702,153.0736333?units=si';
+
+    request({url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service', undefined)
+        } else if (body.error) {
+            callback('Unable to find locations weather', undefined)
+        } else {
+            callback(undefined, `It is currently ${body.currently.temperature} degrees out. With a ${body.currently.precipProbability}% chance of rain.`)
+        }
+    })
+}
+
+module.exports = foreCast
