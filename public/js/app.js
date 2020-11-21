81a1abd8ba0dbe41fcad5e37e28cf8d3b5326ca6
John <john@pebbledesign.com.au>
2020-11-21 16:24:42 +1000

    Duck updates

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,7 @@ const loader = document.querySelector('p.load')
 const messageOne = document.querySelector('p.p1')
 const messageTwo = document.querySelector('p.p2')
 const title = document.querySelector('.title')
+const temp = document.querySelector('.temp')
 const goodDuck = document.querySelector('img.goodDuck')
 const notGoodDuck = document.querySelector('img.notGoodDuck')
 
@@ -15,11 +16,13 @@ messageTwo.textContent = ''
 
 const duck = (duck) => {
     if (duck == true) {
+        title.style.display = 'block'
         title.textContent = "Good Weather for Ducks"
         notGoodDuck.style.display = 'none';
         goodDuck.style.display = 'block';
         
     } else {
+        title.style.display = 'block'
         title.textContent = "Not Good Weather for Ducks"
         notGoodDuck.style.display = 'block';
         goodDuck.style.display = 'none';
@@ -29,6 +32,10 @@ const duck = (duck) => {
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     loader.style.display = 'block'
+    title.style.display = 'none'
+    temp.style.display = 'none'
+    goodDuck.style.display = 'none'
+    notGoodDuck.style.display = 'none'
     messageOne.textContent = ''
     messageTwo.textContent = ''
     const location = searchEl.value
@@ -51,7 +58,11 @@ form.addEventListener('submit', (e) => {
                 } else {
                     duck(true);
                 }
-                console.log(data.data.body.currently.precipProbability)
+                if (data.data.body.currently.temperature >= 25) {
+                    temp.style.display = 'block'
+                    temp.textContent = 'Bit hot out...'
+                }
+                console.log(data.data.body.currently)
             }
         })
     })
